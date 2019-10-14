5d28dd8079e8f8a42caa7c82ff3d589eb282a0f0
EJ Mason <ej-mason@pluralsight.com>
2019-10-14 17:43:31 -0600

    dev

--- LKcreate/index.js
+++ LKcreate/index.js
@@ -1,54 +1,21 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const got = require('got')
-const git = require('simple-git')
 
 const prefix = 'https://spacecat.leankit.com/card/'
 
 async function run() {
     try {
-        const myToken = core.getInput('token')
-        const octokit = new github.GitHub(myToken)
-
-        const { data: pullRequest } = await octokit.pulls.list({
-            owner: 'EJMason',
-            repo: 'test-dependabot',
-            state: 'open',
-        })
-
-        const goodPrs = pullRequest.filter((pri) => pri.labels.filter((label) => label.name === 'dependabot').length > 0)
-
-        if (!goodPrs) {
-            core.setFailed(`There are no PRs that need to be triaged!`)
-        }
-
-        if (goodPrs.length > 1) {
-            core.setFailed('Too many good Pull Requests!')
-        }
-
-        const pr = goodPrs[0]
-
-        const branch = pr.head.ref
-
-        const commits = await octokit.pulls.listCommits({
-            owner: 'EJMason',
-            repo: 'test-dependabot',
-            pull_number: pr.number,
-        })
-
-        console.log('=========== commits ==============')
-        console.log(commits.data[0].commit)
-
         const card_type = core.getInput('lkType') // Card type - defect/risk
         const lane = core.getInput('lkLane') // triage lane id
         const lk_url = core.getInput('lkUrl') // triage lane id
         const lk_board = core.getInput('lkBoard') // triage lane id
         const lk_token = core.getInput('lkToken')
 
+        const pr_title = core.getInput('pr_title')
+        const pr_description = core.getInput('pr_description')
+        const pr_external_link = core.getInput('pr_external_link')
+
         const header = 'Dependabot'
-        const title = pr.title.replace(':robot: ', '')
-        const description = pr.body
-        const external_link = pr.html_url
         const external_link_title = 'Pull Request'
 
         // create the card
@@ -59,29 +26,20 @@ async function run() {
             },
             body: {
                 boardId: lk_board, // required
-                title: title, // required
+                title: pr_title, // required
                 typeId: card_type,
                 laneId: lane,
-                description: description,
+                description: pr_description,
                 externalLink: {
                     label: external_link_title,
-                    url: external_link,
+                    url: pr_external_link,
                 },
                 customId: header,
             },
             json: true,
         })
 
-        const newCommit = commits.data[0].commit.message + `\n\n${prefix}${response.body.id}`
-        console.log('-------------------------------------')
-        console.log(newCommit)
-        await git()
-            .checkout(branch)
-            .commit(newCommit, { '--amend': null })
-            .push('origin', branch, { '--force': null })
-
         core.setOutput('lk_url', `${prefix}${response.body.id}`)
-        core.setOutput('new_commit', newCommit)
     } catch (error) {
         core.setFailed(`Action failed, ${error}`)
     }
